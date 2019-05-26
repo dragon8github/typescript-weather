ead654b6b072be75468b9c5ded33c65c99cb5c96
lizhaohong <928532756@qq.com>
2019-05-26 13:41:13 +0800

    +

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello Typescript')
+const fs = require('fs');
+const program = require('commander');
+const pkg = require('../package.json');
+
+program
+  .version(pkg.version)
+  .description(pkg.description)
+  .usage('[options] <command> [...]')
+  .option('-o, --host <hostname>', 'hostname [localhost]', 'localhost')
+  .option('-p, --port <number>', 'port number [9200]', '9200')
+  .option('-j, --json', 'format output as JSON')
+  .option('-i, --index <name>', 'which index to use')
+  .option('-t, --type <type>', 'default type for bulk operations');
+
+program.parse(process.argv);
+
+if (!program.args.filter((arg: []) => typeof arg === 'object').length) {
+  program.help();
+}
